d40ee193b2898c2f8673269b3a77a79d07f9d55d
Uriel Corfa <korfuri@gmail.com>
2019-05-27 17:51:56 +0900

    Migrate to v1: steps 1-2

--- src/Milkrack.hpp
+++ src/Milkrack.hpp
@@ -1,10 +1,10 @@
-#include "rack0.hpp"
+#include "rack.hpp"
 
 
 using namespace rack;
 
 // Forward-declare the Plugin, defined in Template.cpp
-extern Plugin *plugin;
+extern Plugin *pluginInstance;
 
 // Forward-declare each Model, defined in each module source file
 extern Model *modelWindowedMilkrackModule;
